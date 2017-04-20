4077150b528b5d782f79b9c02d66ef0ec7de83c5
weekdawn <571426779@qq.com>
2017-04-20 16:02:42 +0800

    add threading module to accept muti-client

--- sock.py
+++ sock.py
@@ -1,18 +1,29 @@
 import socket
+import threading
+
+def tcpLick(sock, addr):
+	ip,port = addr
+	print 'start a new connecting from %s:%s' % addr
+	while True:
+		data = sock.recv(1024)
+		print "client%s:%s" % (port, data)
+		if data == 'q':
+			sock.send(data)
+			break
+		else:
+			sock.send(data)
+	sock.close()
+	print 'closed a connection from %s:%s' % addr
 
 s = socket.socket()
 s.bind(('127.0.0.1',8080))
 s.listen(5)
 
-conn, addr = s.accept()
-print "connecting..."
+print "waiting for connecting..."
 while True:
-	data = conn.recv(1024)
-	print 'client:%s' % data
-	if data == 'q':
-		conn.send(data)
-		break
-	conn.send(data)
+	conn, addr = s.accept()
+	t = threading.Thread(target=tcpLick, args=(conn,addr))
+	t.start()
 conn.close()
 print 'connecting is closed'
 
